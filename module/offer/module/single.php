5535567a419db01caebd876c4f576913585db813
Ewa <biuro@sadzonka.eu>
2016-04-10 14:28:42 +0200

    poprwa odslony rosliny

--- module/offer/module/single.php
+++ module/offer/module/single.php
@@ -6,13 +6,13 @@ class Offer_Module_Single extends Module_Abstract {
     function execute() {
         $this->in['idCategory'] = (int) $this->request->get(Model_Plant_Category_Container::FILTER);
         $this->in['id'] = (int) $this->request->get(Model_Plant_Container::FILTER);
-        
+                
         if ($this->in['id'] <= 0) {
             throw new Manager_Exception_NotFound();
         }
-
         try {
-            $this->out['category'] = $this->storage->getParam('list-nav')->search('id', $this->in['id']);
+            $this->out['category'] = $this->storage->getParam('list-nav')->search('id', $this->in['idCategory']);
+            
             if ($this->out['category'] instanceof Model_Link_Container) {
 
                 $api = new Model_Plant_Source_Api(Model_Plant_Source_Factory::DB_MYSQL);
@@ -43,8 +43,11 @@ class Offer_Module_Single extends Module_Abstract {
                 throw new Manager_Exception_NotFound();
             }
         } catch (Model_Exception_NotFound $e) {
+            
+            //echo $e->getMessage();exit;
             throw new Manager_Exception_NotFound();
         } catch (Exception $e) {
+             //echo $e->getMessage();exit;
             throw new Manager_Exception_Unavailable();
         }
         $this->template = 'Offer/View/Single.phtml';
